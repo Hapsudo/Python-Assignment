5f3394f93db91882c48667fc0dbc3f53cae55b1f
Hapsudo <catherine.abugah8@gmail.com>
2025-04-02 01:01:42 +0300

    Submitting week 2 Assignment

---
+++ wk2.py
@@ -0,0 +1,28 @@
+#Question 1
+my_list = []
+
+#Question 2
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+#Question 3
+my_list.insert(1,15)
+print(my_list)
+
+#Question 4
+my_list.extend([50,60,70])
+print(my_list)
+
+#Question 5
+my_list.remove(my_list[-1])
+print(my_list)
+
+#Question 6
+my_list.sort()
+print(my_list)
+
+#Question 7
+print(my_list[3])
